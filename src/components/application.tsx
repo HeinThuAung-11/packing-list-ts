9d8a4f9ce7d1f1b853fb8d9e13e00710e0b9bfea
Hein1P <heinaungsint2001@gmail.com>
2023-06-27 23:22:20 +0630

    add uuid

---
+++ src/components/application.tsx
@@ -0,0 +1,7 @@
+export const Application = () => {
+  return (
+    <h1 className="text-3xl font-bold underline text-red-600">
+      Simple React Typescript Tailwind Sample
+    </h1>
+  )
+}
---
+++ src/libs/items.ts
@@ -0,0 +1,44 @@
+import { v4 as id } from 'uuid';
+
+export const createItem = (name: string): Item => {
+  return {
+    id: id(),
+    name,
+    packed: false,
+  };
+};
+
+let items = [
+  'Sweatshirt',
+  'Running shoes',
+  'AirPods',
+  'MacBook',
+  'iPad',
+  'USB-C cable',
+  'Lightning cable',
+  'Wallet',
+  'MagSafe cable',
+  'Apple Watch charger',
+  'Power brick',
+  'Toothbrush',
+  'Toothpaste',
+  'Deorderant',
+  'Backpack',
+  'Vitamins',
+  'Kindle',
+  'Micro-USB cable',
+  'Sleep mask',
+  'Ear plugs',
+  'Face masks',
+  'Sony Walkman',
+  'Emergency Vegan Bacon',
+].map(createItem);
+
+const [first, second] = items;
+
+first.packed = true;
+second.packed = true;
+
+export const getInitialItems = (): Item[] => {
+  return items;
+};
